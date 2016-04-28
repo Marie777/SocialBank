074ab5e1b8b241f014855f1a5193028cc391cbef
Mmarie <mastula.net@gmail.com>
2016-04-28 07:34:46 -0700

    Finished customer entity

--- social_bank/src/AppBundle/Entity/Customer.php
+++ social_bank/src/AppBundle/Entity/Customer.php
@@ -28,8 +28,72 @@ class Customer extends User {
 
     /**
      * @Column(type="enummaritalstatus")
+     * @var string
      */
     protected $status;
 
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * @param \DateTime $dateOfBirth
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJobDescription()
+    {
+        return $this->jobDescription;
+    }
+
+    /**
+     * @param string $jobDescription
+     */
+    public function setJobDescription($jobDescription)
+    {
+        $this->jobDescription = $jobDescription;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
 
 }
