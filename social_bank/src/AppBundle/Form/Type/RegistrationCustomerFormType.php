bf41a2c30a7035076ff5f06076395ea016bc7471
Mmarie <mastula.net@gmail.com>
2016-04-29 00:17:42 -0700

    Add account registration type

---
+++ social_bank/src/AppBundle/Form/Type/RegistrationCustomerFormType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+
+use AppBundle\DBAL\MaritalStatus;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationCustomerFormType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('name', TextType::class)
+            ->add('dateOfBirth', DateType::class)
+            ->add('jobDescription', TextType::class)
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'Married' => MaritalStatus::MARRIED,
+                    'Single' => MaritalStatus::SINGLE,
+                    'Divorced' => MaritalStatus::DIVORCED
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ])
+            ->add('submit', SubmitType::class);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Customer',
+        ]);
+    }
+}
