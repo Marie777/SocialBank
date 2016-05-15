26dbbf43dd8d86183d86295c337cd1562636aff1
Mmarie <mastula.net@gmail.com>
2016-05-15 11:14:00 -0700

    add report controller

---
+++ social_bank/src/AppBundle/Controller/ReportController.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mmarie
+ * Date: 5/15/2016
+ * Time: 9:06 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Report;
+use AppBundle\Entity\Transaction;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+
+class ReportController extends Controller
+{
+
+    /**
+     * @Route("/report", name="get-report-list")
+     * @Security("has_role('ROLE_USER')")
+     * @Template
+     */
+    public function getAllAction() {
+
+        $reports = $this->getDoctrine()->getManager()->getRepository(Report::class);
+
+        return [
+            "reports" => $reports
+        ];
+    }
+
+    /**
+     * @Route("/report/create", name="create-report")
+     * @param Request $request
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function createAction(Request $request){
+
+        $om = $this->getDoctrine()->getManager();
+
+        $report = new Report();
+
+        $form = $this->createForm(CreateReportFormType::class, $report);
+        $form->handleRequest($request);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $om->persist($report);
+            $om->flush();
+            return $this->redirectToRoute('get-report-list');
+        }
+        /** @var TYPE_NAME $form */
+        return ['form' => $form->createView()];
+
+    }
+
+    /**
+     * @Route("/report/{id}/display", name="display-report")
+     * @Template
+     * @param Report $report
+     * @return array
+     */
+    public function displayAction(Report $report){
+        $om = $this->getDoctrine()->getManager();
+        $allTransactions = $om->getRepository(Transaction::class)
+                              ->findRangeDate($report->getStartDate(),$report->getEndDate());
+        
+        
+        $countTransactions = count($allTransactions);
+        $maxAmountTransaction = max($allTransactions->getAmount);
+
+
+        return [
+           "countTransactions"=> $countTransactions,
+           "maxAmountTransaction" => $maxAmountTransaction
+        ];
+    }
+
+}
